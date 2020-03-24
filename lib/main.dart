ad14ca5c64a80d1da3f8c121ed7899b55e700926
chunleethong <thong.chunlee@optistech.com>
2020-03-24 16:50:55 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_movie_app/pages/home_page/home_page.dart';
+
+void main() {
+  return runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Movie App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: "GoogleSans",
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
